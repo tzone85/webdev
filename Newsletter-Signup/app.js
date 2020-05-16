d236cb7421adf469a23a1d59cb539782b2566a6e
Thando Mini <tzone85@gmail.com>
2020-05-16 17:41:24 +0200

    made a simple post request to the home route using body-parser

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -8,7 +8,6 @@ var path = require('path');
 const app = express();
 
 //to render static content/pages
-// app.use(express.static("public"));
 app.use('*/image',express.static(path.join(__dirname, 'public/image')));
 app.use('*/js',express.static(path.join(__dirname, 'public/javascripts')));
 app.use('*/css',express.static(path.join(__dirname, 'public/css')));
@@ -17,7 +16,15 @@ app.use(bodyParser.urlencoded({extended:true}) );
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/signup.html"));
-})
+});
+
+app.post("/", function (req, res) {
+   var firstName = req.body.fName;
+   var lastName = req.body.lName;
+   var email = req.body.email;
+
+   console.log(firstName, lastName, email);
+});
 
 app.listen(3000, function () {
     console.log("listenning to port 3000");
